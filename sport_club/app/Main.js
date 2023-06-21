6bace610a1a9c3c14ee0a00775ad0c571f611e6c
Igor Parfenov <lloopy123123543@gmail.com>
2023-06-21 16:45:00 +0500

    start

---
+++ sport_club/app/Main.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>
+        <div className="container w-full mx-auto p-20">
+<h1 class="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Get back to growth with <span class="text-blue-600 dark:text-blue-500">the world's #1</span> CRM.</h1>
+<p class="text-lg text-center font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        </div>
+    </div>
+  )
+}
---
+++ sport_club/app/page.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+import Header from './header'
+import Main from './Main'
+import Assorti from './assorti'
+export default function Home() {
+  return (
+    <div className='w-full'>
+      <Header></Header>
+      <Main></Main>
+      <Assorti></Assorti>
+
+    </div>
+  )
+}
